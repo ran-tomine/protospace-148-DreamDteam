9fa1783b7a015a8c766208310f7b5aaddff69cab
ran02160 <ran02160ay@gmail.com>
2023-05-22 12:29:16 +0900

    編集機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,6 +16,20 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+    
   private
 
   def prototype_params
