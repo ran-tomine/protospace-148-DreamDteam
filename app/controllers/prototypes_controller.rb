4158a3328892ab79ec4adc3b47cda3f048cd40ee
mylrella1102 <tmk12.3u@gmail.com>
2023-05-22 19:29:38 +0900

    最新の順番で表示に変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.all.order("created_at DESC")
   end
 
   def new
