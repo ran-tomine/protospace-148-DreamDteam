914f84af0a16d20a7a0c2198d4293a057d721f44
mylrella1102 <tmk12.3u@gmail.com>
2023-05-22 14:01:38 +0900

    ユーザー詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: "prototypes#index"
 
   resources :prototypes, only: [:new,:create]
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
