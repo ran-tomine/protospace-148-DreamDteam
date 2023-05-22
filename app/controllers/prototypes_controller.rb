a3d7f2e9aed23d9dc2312a705758ff2dbbe260d2
kh00x10a <sein.nicht04@gmail.com>
2023-05-22 17:28:32 +0900

    コメントの表示機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -41,6 +41,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments =@prototype.comments.includes(:user)
   end
 
   private
