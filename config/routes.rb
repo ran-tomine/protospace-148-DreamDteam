0dc1fa6212bbd92fae5b084e153cc7c5fb883bb9
ran02160 <ran02160ay@gmail.com>
2023-05-22 14:01:38 +0900

    コントローラー作成、アクションとルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: "prototypes#index"
 
   resources :prototypes, only: [:new,:create]
+
+  resources :comments, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
